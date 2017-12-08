e7168d17e5b3fa7b3380f7d6b113f581c9dbb2e5
Jonathan Zarate <zaratedev@gmail.com>
2017-12-08 12:08:40 -0600

    Add Position, margin and Filling

--- app/src/main/java/com/zaratedev/myfirstapp/MainActivity.java
+++ app/src/main/java/com/zaratedev/myfirstapp/MainActivity.java
@@ -6,33 +6,32 @@ import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Create a linearLayout
+        //create new linearLayout
         LinearLayout linearLayout = new LinearLayout(getApplicationContext());
 
-        //Configuration linearLayout
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        linearLayout.setBackgroundColor(Color.BLUE);
-
-        //set linearLayout to ContentView
         setContentView(linearLayout);
 
-        //Add textView
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        //create new textView
         TextView textView = new TextView(getApplicationContext());
-        textView.setText("Hello world");
+        textView.setText("Hello World");
 
-        //textView.setLayoutParams(params);
+        //Create layout params
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        //set margin to params
+        params.leftMargin = 30;
+        //Add padding to textView
+        textView.setPadding(0,20,0,0);
+        //Add color backgroundColor to textView
         textView.setBackgroundColor(Color.RED);
-
-        //set textView to LinearLayout
+        //add view textView to linearLayout
         linearLayout.addView(textView, params);
+
     }
 
 }
