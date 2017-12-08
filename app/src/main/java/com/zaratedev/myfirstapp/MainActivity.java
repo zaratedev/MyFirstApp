e6ff2010b567e1f44a73a2a02a2bc6b5498e4fba
Jonathan Zarate <zaratedev@gmail.com>
2017-12-08 12:35:09 -0600

    Loading resources to view

--- app/src/main/java/com/zaratedev/myfirstapp/MainActivity.java
+++ app/src/main/java/com/zaratedev/myfirstapp/MainActivity.java
@@ -3,34 +3,19 @@ package com.zaratedev.myfirstapp;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        //create new linearLayout
-        LinearLayout linearLayout = new LinearLayout(getApplicationContext());
-
-        setContentView(linearLayout);
-
-        //create new textView
-        TextView textView = new TextView(getApplicationContext());
-        textView.setText("Hello World");
-
-        //Create layout params
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        //set margin to params
-        params.leftMargin = 30;
-        //Add padding to textView
-        textView.setPadding(0,20,0,0);
-        //Add color backgroundColor to textView
-        textView.setBackgroundColor(Color.RED);
-        //add view textView to linearLayout
-        linearLayout.addView(textView, params);
+        button = (Button) findViewById(R.id.button);
+        button.setBackgroundColor(Color.GREEN);
 
     }
 
